f8b901f00023a85f981493ee87e2e998fe99fa7a
Jacob Socolar <jacob.socolar@gmail.com>
2019-01-07 18:53:35 -0600

    making ricklefs_analysis into an R package

--- Scripts/ricklefs_analysis.R
+++ Scripts/ricklefs_analysis.R
@@ -1,3 +1,15 @@
+#' Compute whether lineages that characteristically occur at species-rich sites have different average richness that lineages that characteristically occur at species-poor sites
+#'
+#' @param assigned_lineages output from \code{\link{lineage_assign}}
+#' @export
+#' @examples
+#' library(phylosympatry)
+#' tree <- ape::rtree(100)
+#' lineages <- get_lineages(tree,1)
+#' species_prop <- runif(nrow(lineages))
+#' assigned_lineages <- lineage_assign(lineages,species_prop,.7,.1,.3,.2)
+#' ricklefs_analysis(assigned_lineages)
+
 ricklefs_analysis <- function(assigned_lineages){
   nlin <- max(assigned_lineages$ID)
   df <- as.data.frame(matrix(data=NA, nrow=nlin, ncol=2))
