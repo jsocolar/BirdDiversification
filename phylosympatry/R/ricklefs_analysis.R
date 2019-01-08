f60d9ce5e6e1886af9e3bb2a40bd7dcfd912f647
Jacob Socolar <jacob.socolar@gmail.com>
2019-01-08 12:18:28 -0600

    added sample sizes to ricklefs output

--- phylosympatry/R/ricklefs_analysis.R
+++ phylosympatry/R/ricklefs_analysis.R
@@ -18,11 +18,12 @@ ricklefs_analysis <- function(assigned_lineages){
     df$richness[i] <- sum(assigned_lineages$ID == as.character(i)) 
     df$class[i] <- unique(assigned_lineages$lineage_class[assigned_lineages$ID == as.character(i)])
   }
+  n <- table(df$class)
   means <- list(R = mean(df$richness[df$class=="R"]), P = mean(df$richness[df$class=="P"]))
   medians <- list(R = median(df$richness[df$class=="R"]), P = median(df$richness[df$class=="P"]))
   df2 <- df[df$class %in% c("R", "P"), ]
   wilcox <- wilcox.test(richness ~ class, data = df2)
   
-  output <- list(df=df, means=means, medians=medians, wilcox=wilcox)
+  output <- list(df=df, n=n, means=means, medians=medians, wilcox=wilcox)
   return(output)
 }
